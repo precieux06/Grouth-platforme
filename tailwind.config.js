99ef6c290b2466668a454acb8ac5a5b14775602d
precieux06 <jmakumbusela@gmail.com>
2025-09-18 21:58:49 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}", "./app/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        neon: '#00fff7',
+        bg1: '#0a0a14',
+        panel: '#121228'
+      }
+    }
+  },
+  plugins: [],
+      }
+    
